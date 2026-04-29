576425b70f739897f6dcc2d150bc53e63b96076a
Taylor Swanson <taylor.swanson@elastic.co>
2026-04-29 08:41:21 -0500

    use short license header, restore main package comment

--- main.go
+++ main.go
@@ -2,6 +2,8 @@
 // Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+// The package-tool command provides a number of useful utilities for managing
+// Elastic integration packages.
 package main
 
 import (
